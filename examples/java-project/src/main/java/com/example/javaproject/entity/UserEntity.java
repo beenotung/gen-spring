9cb75be658b222742c75b40aeae46df7d32d87fa
Beeno Tung <aabbcc1241@yahoo.com.hk>
2023-10-22 04:16:40 +0800

    chore: update quick-erd to generate entity with 4 spaces indentation and explicitly state column name of primary key

--- examples/java-project/src/main/java/com/example/javaproject/entity/UserEntity.java
+++ examples/java-project/src/main/java/com/example/javaproject/entity/UserEntity.java
@@ -1,35 +1,31 @@
 package com.example.javaproject.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Column;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "`user`")
 public class UserEntity {
-  @Id
-  @GeneratedValue
-  @Column
-  private Long id;
+    @Id
+    @GeneratedValue
+    @Column(name = "`id`")
+    private Long id;
 
-  @Column(name = "`username`", nullable = false)
-  private String username;
+    @Column(name = "`username`", nullable = false)
+    private String username;
 
-  public Long getId() {
-    return id;
-  }
+    public Long getId() {
+        return id;
+    }
 
-  public void setId(Long id) {
-    this.id = id;
-  }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-  public String getUsername() {
-    return username;
-  }
+    public String getUsername() {
+        return username;
+    }
 
-  public void setUsername(String username) {
-    this.username = username;
-  }
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
